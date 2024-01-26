29e8928989d79e91cd50c3f201b77ff1741433d7
Jeelpatel0903 <71834522+Jeelpatel0903@users.noreply.github.com>
2024-01-26 13:26:27 +0530

    add count

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,15 +10,31 @@ import { BranchService } from '../services/branch.service';
 })
 export class HomeComponent implements OnInit {
 
+  val:number = 0
+  total:number[]=[]
   constructor(private user:UserService,
     private company:CompnayService,
     private branch:BranchService) { }
 
   ngOnInit(): void {
+    this.branch.branchDetails.forEach(e=>{
+      // console.log(e);
+      this.total.push(Number(e.branchCount))
+      // console.log(this.total);
+      
+      // console.log(this.val += Number(e.branchCount));
+      
+    })
+    for (let index = 0; index < this.total.length; index++) {
+      this.val = this.val + this.total[index]
+      
+    }
   }
 
   countemployee = this.user.user.length
   countcompany = this.company.companyDetails.length
   countbranch = this.branch.branchDetails.length
+  
+
 
 }
